095abc84f9fc6d66cd763afe9a30aa88c8f504a9
jliocsar <juliocesarmfo@gmail.com>
2024-10-26 18:26:21 -0300

    fix install script

--- install.sh
+++ install.sh
@@ -6,6 +6,7 @@ curl -sSfL $timov_url > $HOME/.timov.sh
 chmod +x $HOME/.timov.sh
 
 echo "Adding Timov to crontab..."
-echo "$(crontab -l)\n*/2 * * * * $HOME/.timov.sh >> /tmp/timov.log" | crontab -
+cur_cron=$(crontab -l | sed -e "s@.*timov\.sh.*@@g")
+echo "$($cur_cron)\n*/2 * * * * $HOME/.timov.sh >> /tmp/timov.log" | crontab -
 
 echo "All done!"
